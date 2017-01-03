8c7b8d3e8cc5c62aac1aa363bf58afdaa16b8e39
evuz <jesus_jgb_45@hotmail.com>
2017-01-03 22:38:33 +0100

    Hello World React Component & Display Output in React with a Component's render Method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React from 'react'
 
-const App = () => <h1>Hi!</h1>
+class App extends React.Component {
+  render () {
+    // return React.createElement('h1', null, 'Hello World!');
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <b>Bold</b>
+      </div>
+    )
+  }
+}
+
+// const App = () => <h1>Hello stateless</h1>
 
 export default App;
